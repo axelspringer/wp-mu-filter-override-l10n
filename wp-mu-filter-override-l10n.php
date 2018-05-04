7122e532ebc61ff3cfdabe160109358b99ca94b5
Mathias Witt <mathias.witt@axelspringer.de>
2018-05-04 09:39:47 +0200

    renaming namespace, filename and update composer file

--- wp-mu-filter-override-l10n.php
+++ wp-mu-filter-override-l10n.php
@@ -4,38 +4,38 @@
 
 /**
  * Cache translation
+ *
  * @param boolean $override
- * @param string  $domain
- * @param string  $mofile
+ * @param string $domain
+ * @param string $mofile
+ *
  * @return boolean
  */
-function my_override_load_textdomain($override, $domain, $mofile) {
+function custom_override_load_textdomain( $override, $domain, $mofile ) {
 
     global $l10n;
 
 
     // check if $mofile exisiste and is readable
-    if ( !(is_file($mofile) && is_readable($mofile)) ){
+    if ( ! ( is_file( $mofile ) && is_readable( $mofile ) ) ) {
         return false;
     }
 
-    //var_dump($mofile);
-
     // creates a unique key for cache
-    $key   = md5($mofile);
+    $key = md5( $mofile );
 
     // I try to retrive data from cache
-    $data  = wp_cache_get($key, $domain);
+    $data = wp_cache_get( $key, $domain );
 
     // Retrieve the last modified date of the translation files
-    $mtime = filemtime($mofile);
+    $mtime = filemtime( $mofile );
 
     $mo = new \MO();
 
     // if cache not return data or data it's old
-    if ( !$data || !isset($data['mtime']) || ($mtime > $data['mtime']) ) {
+    if ( ! $data || ! isset( $data['mtime'] ) || ( $mtime > $data['mtime'] ) ) {
         // retrive data from MO file
-        if ( $mo->import_from_file( $mofile ) ){
+        if ( $mo->import_from_file( $mofile ) ) {
 
             $data = array(
                 'mtime'   => $mtime,
@@ -44,7 +44,7 @@ function my_override_load_textdomain($override, $domain, $mofile) {
             );
 
             // save data in cache
-            wp_cache_set($key, $data, $domain, 0);
+            wp_cache_set( $key, $data, $domain, 0 );
 
         } else {
             return false;
@@ -55,13 +55,13 @@ function my_override_load_textdomain($override, $domain, $mofile) {
         $mo->headers = $data['headers'];
     }
 
-    if ( isset( $l10n[$domain] ) ) {
-        $mo->merge_with( $l10n[$domain] );
+    if ( isset( $l10n[ $domain ] ) ) {
+        $mo->merge_with( $l10n[ $domain ] );
     }
 
-    $l10n[$domain] = &$mo;
+    $l10n[ $domain ] = &$mo;
 
     return true;
 }
 
-add_filter('override_load_textdomain', 'my_override_load_textdomain', 1, 3);
+add_filter( 'override_load_textdomain', 'custom_override_load_textdomain', 1, 3 );
